b335328107d7cf8086ecdd9a9f0cd0d94eaf254f
qcluu <lcao29@gmail.com>
2023-11-24 19:45:57 -0500

    Codex initial implementation
    Co-authored-by: aethyme <aethyme@users.noreply.github.com>
    Co-authored-by: Adam Green <ItsmeCurly@users.noreply.github.com>

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -3,11 +3,14 @@ import Phaser from "phaser";
 import PlayerInventory from "../ui/Inventory";
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { TestMyCodex } from "../components/TestMyCodex"
+import MycoDex from "../ui/MycoDex"
 import game from "../game";
 
 export default class MainScene extends Phaser.Scene {
   mushrooms = require("../data/mushroom.json");
+  private inventory: PlayerInventory;
+  // private codex: MycoDex;
+
   constructor() {
     super({ key: "MainScene" });
   }
@@ -23,7 +26,11 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
+    this.inventory = new PlayerInventory(this, Number(this.sys.game.config.width) / 2 - 800 /2, Number(this.sys.game.config.height) / 2 - 400/2, 800, 400, 4, 8);
+    this.inventory.setVisible(false);
 
+    const codex = new MycoDex(this, Number(this.sys.game.config.width) / 2, Number(this.sys.game.config.height) / 2, 400, 600);
+    codex.setVisible(false);
 
     // Eventually, when we have 5 mushrooms we can change this.mushrooms.length to 5 or however many mushrooms we want
     for (let i = 0; i < this.mushrooms.length; i++) {
@@ -68,33 +75,19 @@ export default class MainScene extends Phaser.Scene {
       });
     }
 
-    const codexSprite = this.add.sprite(150, 100, "journal").setScale(0.25).setInteractive();
 
-    codexSprite.on("pointerup", function (pointer) {
-      const reactRoot = document.createElement('div');
-      document.body.appendChild(reactRoot);
+    const codexObj = this.add.image(150, 50, 'journal').setScale(0.25).setInteractive();
 
-      const [codexVisible, setCodexVisible] = useState(false);
-
-      let toggleCodexVisibility = () => {
-        setCodexVisible(!codexVisible);
-      }
-
-      ReactDOM.render(
-        <TestMyCodex visible={codexVisible} toggleCodexVisibility={toggleCodexVisibility} />,
-        reactRoot
-      )
+    codexObj.on('pointerup', function(pointer) {
+      codex.toggleBookVisibility()
     })
 
-    const inventory = new PlayerInventory(this, Number(this.sys.game.config.width) / 2 - 800 /2, Number(this.sys.game.config.height) / 2 - 400/2, 800, 400, 4, 8)
-    inventory.visible = false
 
     const bagSprite = this.add.sprite(50, 50, "bag").setScale(0.25).setInteractive();
     
     bagSprite.on("pointerup", function (pointer) {
-      inventory.visible = !inventory.visible
-      inventory.bringToFront()
-      // Mycodex.setShowCodex(true);
+      this.inventory.visible = !this.inventory.visible
+      this.inventory.bringToFront()
     });
 
     //Event listeners
